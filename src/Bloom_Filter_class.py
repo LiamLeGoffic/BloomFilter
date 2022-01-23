8ecd01c46e064ebd291883d44818e82cfa7e7569
LiamLeGoffic <liamlegoffic@yahoo.fr>
2022-01-23 20:12:36 +0100

    Update Bloom_Filter_class.py

--- src/Bloom_Filter_class.py
+++ src/Bloom_Filter_class.py
@@ -13,13 +13,13 @@ def xorshift64(x):
 # input: bf -> object of BloomFilter class
 #        x  -> value to hash
 #        k  -> size of kmers
-# output: index generator indicating a cell of the Bloom filter
+# output: index generator indicating a cell of the Bloom filter and the corresponding bit
 def multihash(bf, x, k):
     if x==0:
         x=sum([2**i for i in range(1, 2*k, 2)])  # replace x by the code of the reverse sequence if x is 0
     for i in range(bf.nf):
         x = xorshift64(x)
-        yield x%bf.size
+        yield x%bf.size, x%8
 
 # Class of Bloom Filter
 # attributes: n    ->  size of the filter
@@ -35,20 +35,20 @@ class BloomFiltrer:
         self.n = n
         self.nf = nf
         self.size = int(n/8)+1
-        self.array = [0]*self.size
+        self.array = bytearray(self.size)
     
     # Add a value to the filter
     # input: x -> value to add
     #        k -> size of kmers
     def add_value(self, x, k):
-        for ind in multihash(self, x, k):
-            self.array[ind]=1
+        for ind,bit in multihash(self, x, k):
+            self.array[ind]=1<<bit
     
     # Verify if a value is in the filter
     # input: x -> value to add
     #        k -> size of kmers
     # output: True if x is in the filter, False otherwise
     def is_present(self, x, k):
-        for ind in multihash(self, x, k):
-            if self.array[ind]!=1: return False
+        for ind,bit in multihash(self, x, k):
+            if self.array[ind]&(1<<bit)==0: return False
         return True
